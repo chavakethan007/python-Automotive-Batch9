c0a318441384ed4ed465b279a37e62e7f6607731
ChavaKethan <chavakethan007@gmail.com>
2025-12-16 09:45:22 +0530

    string manipulation done

---
+++ Python/sm3.py
@@ -0,0 +1,5 @@
+list=[1,2,3]
+mylist=[1,2,3]
+print("A list is: %s" % mylist)
+"""% is a string formatting operator used to insert values into a string. 
+%s replaces itself with the string representation of the given value."""
