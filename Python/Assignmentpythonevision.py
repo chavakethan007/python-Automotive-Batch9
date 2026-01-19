19d851d2610f8f4388ab990ab037abd691a05130
ChavaKethan <chavakethan007@gmail.com>
2026-01-19 17:29:51 +0530

    Assignment python revision

---
+++ Python/Assignmentpythonevision.py
@@ -0,0 +1,35 @@
+"""1. Reverse a String
+Question:
+Write a Python program to reverse a given string without using built-in reverse functions."""
+
+n=input()
+reverse=n[::-1]
+print(reverse)
+
+"""2. Check Prime Number
+Question:
+Write a program to check whether a given number is a prime number or not."""
+
+m=int(input())
+for i in range(m):
+    
+
+"""3. Find Largest Element in a List
+Question:
+Write a Python program to find the largest number in a list without using max().
+
+4. Count Vowels in a String
+Question:
+Write a program that counts the number of vowels (a, e, i, o, u) in a given string.
+
+5. Fibonacci Series
+Question:
+Write a Python program to generate the Fibonacci series up to n terms.
+
+6. Remove Duplicates from a List
+Question:
+Write a program to remove duplicate elements from a list while maintaining the original order.
+
+7. Check Palindrome
+Question:
+Write a Python program to check whether a given string or number is a palindrome."""
