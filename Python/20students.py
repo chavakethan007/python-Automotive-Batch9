3a4be863e62ac286ef227bbd35b84cc8e3d99099
ChavaKethan <chavakethan007@gmail.com>
2026-01-19 17:34:53 +0530

    same first name with different surnames

--- Python/20students.py
+++ Python/20students.py
@@ -1,4 +1,8 @@
-students={"chava": "kethan",
+# Dictionary to store students
+# Key   -> Surname
+# Value -> First name
+students = {
+    "chava": "kethan",
     "bathi": "pardhiv",
     "chevi": "pradeep",
     "gunupudi": "chetan",
@@ -17,26 +21,38 @@ students={"chava": "kethan",
     "reddy": "suresh",
     "naidu": "suresh",
     "sharma": "rahul",
-    "verma": "rahul"}
+    "verma": "rahul"
+}
 
-'''for i in range(1,21):
-    surname=input("Enter surname: ")
-    name=input("Enter name: ")
 
-    students[surname]=name'''
+# Optional code to take input from user
+# (Currently commented because data is already given)
+'''
+for i in range(1, 21):
+    surname = input("Enter surname: ")
+    name = input("Enter name: ")
+    students[surname] = name
+'''
+# Print all students (surname and name)
+for surname, name in students.items():
+    print({surname}, {name})
 
-for surname,name in students.items():
-    print({surname},{name})
+# Dictionary to count names
+# Key   -> First name
+# Value -> List of surnames having that name
+name_count = {}
 
-name_count={}
-
+# Group surnames by same first name
 for surname, name in students.items():
     if name in name_count:
         name_count[name].append(surname)
     else:
         name_count[name] = [surname]
 
+# -------------------------------
+# Print only duplicate names
+# (same first name with different surnames)
+# -------------------------------
 for name, surnames in name_count.items():
     if len(surnames) > 1:
         print(name, surnames)
-
